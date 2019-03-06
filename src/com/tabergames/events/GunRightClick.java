f652773dbcf95cad4ed5bc0b1941485bb2114ae8
joshua-robotics <joshuarobotics1@gmail.com>
2019-03-05 21:10:17 -0500

    woOf

--- src/com/tabergames/events/GunRightClick.java
+++ src/com/tabergames/events/GunRightClick.java
@@ -1,21 +1,18 @@
 package com.tabergames.events;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.ListIterator;
+
 import org.bukkit.*;
-import org.bukkit.entity.*;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
-public class GunRightClick {
-    //woOf
-    Player player = event.getPlayer;
-
-        public void onShootClick(Event shoot) {
-        ClickType clickType = shoot.getClick();
-        if (clickType != ClickType.RIGHT) { 
-            return;
-        }
-
-        if (e.getCurrentItem() == null || shoot.getCurrentItem().getType() != Material.DIAMOND_AXE) {
-            return;
-        }
-    }
-
-} 
+public class GunRightClick implements Listener{
+	
+}
