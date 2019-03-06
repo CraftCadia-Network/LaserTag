828fe0fbffbe6482fdf04e40fecb7146602d19ed
Sam Taber <admin@cadianetwork.com>
2019-03-05 22:21:07 -0500

    good

--- src/com/tabergames/events/shoot.java
+++ src/com/tabergames/events/shoot.java
@@ -1,19 +1,34 @@
 package com.tabergames.events;
 
+import java.util.Set;
+
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 import net.jaxonbrown.guardianBeam.beam.Beam;
 
 public class shoot {
 	
-	private Player player;
-	private double end= +5;
 	
- public shoot() {
+	
+@EventHandler
+ public void shoot(PlayerInteractEvent e) {
 	 
-	 Beam beam = new Beam(player.getEyeLocation(), player.);
+	 Player p = e.getPlayer();
+	 Action a = e.getAction();
+	 
+	Location target = p.getTargetBlock((Set) null, 200).getLocation().clone().add(0, 0.3, 0);
+	 
+	 if((a == Action.PHYSICAL) || (e.getItem() == null) || (e.getItem().getType() == Material.DIAMOND_AXE)) return;
+	 Beam beam = new Beam(p.getEyeLocation(), target);
+	 beam.setStartingPosition(p.getEyeLocation());
 	 beam.start();
-	 beam.setStartingPosition(player.getEyeLocation());
+	 
+	
  }
 }
